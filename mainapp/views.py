a69d211082b03834eb6a87b3053985c340f92858
Cholinesterasa@gmail.com <cholinesterasa@gmail.com>
2022-05-03 16:53:34 +0300

    test

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from datetime import datetime
 
 # Create your views here.
 
@@ -8,6 +9,15 @@ class MainPageView(TemplateView):
 
 class NewsPageView(TemplateView):
     template_name = "mainapp/news.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['news_title'] = "ЗАГОЛОВОК"
+        context['news_preview'] = "description"
+        context['range'] = range(5)
+        context['datetime_obj'] = datetime.now()
+        return context
 
 class CoursesPageView(TemplateView):
     template_name = "mainapp/courses_list.html"
