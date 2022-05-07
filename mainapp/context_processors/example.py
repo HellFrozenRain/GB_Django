957e6f0bf99b3bf64a001f7b2dc3770459fb9539
Cholinesterasa@gmail.com <cholinesterasa@gmail.com>
2022-05-07 18:34:38 +0300

    lesson_3

--- mainapp/context_processors/example.py
+++ mainapp/context_processors/example.py
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 def simple_context_processor(request):
     return {"foo": "bar"}
-=======
-def simple_context_processpr(request):
-    return {'foo': 'bar'}
->>>>>>> 8c33b25e1d35563f2522584128f9cfa6cd2d995e
